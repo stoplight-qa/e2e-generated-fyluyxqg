3fd49625b1ca42b6b14903c9eb4d2abb23d8dce7
qa-awola-0-mrkt@example.com <qa-awola-0-mrkt@example.com>
2023-01-31 15:06:30 -0500

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjk0MzEy/spectral.js?branch_slug=main&token=1fc9f0ba-936d-4f6f-a040-6984ea04e459";
+export default { extends: ruleset };
